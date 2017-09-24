1fd10b9928f7c2df16b7e6c09977e535f0a238f8
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-09-24 19:27:10 +0100

    added test files, modified main function

--- function.php
+++ function.php
@@ -7,32 +7,57 @@
  */
 
 $our_user = "Vasile";
-$our_user_pass = "Hello123";
+$our_user_pass = "123";
 
 session_start();
 
-function submit_form ($our_user, $our_user_pass, $post_user_name, $post_password) {
-    if (empty($post_user_name) || empty($post_password)) {
-        return "<p style='color:red;'>Please provide login and pass</p>";
+/*
+ *  submit_form checking inputs to be field
+ * @params $our_user        - user name from database;
+ * @params $our_user_pass   - user password from database;
+ * @params $post_user_name  - provided name by user;
+ * @params $post_password   - provided password by user;
+ *
+ * @return success          - run session_data function to check username and user password
+ * @return fail string      - return request to fill inputs
+ */
+function submit_form($our_user, $our_user_pass, $post_user_name, $post_password) {
+    if (!(empty($post_user_name) || empty($post_password))) {
+        return session_data($our_user, $our_user_pass, $post_user_name, $post_password);
     } else {
-        $user = $post_user_name;
-        $user_pass = $post_password;
-        return session_data($our_user, $our_user_pass, $user, $user_pass);
+        return "<p style='color:red;'>Please provide login and pass</p>";
     }
 }
-function session_data($our_user, $our_user_pass, $user, $user_pass) {
-    if ($our_user === stripslashes($user) && $our_user_pass === stripslashes($user_pass)) {
-        $_SESSION['user'] = $user;
-        $_SESSION['password'] = $user_pass;
-        header("Location: welcome_page.php");
-        exit;
+
+/*
+ * session_data checking provided login and password with database data
+ * @params $our_user        - user name from database;
+ * @params $our_user_pass   - user password from database;
+ * @params $post_user_name  - provided name by user;
+ * @params $post_password   - provided password by user;
+ * @return success          - redirect to welcome page;
+ * @return fail             - message that wrong data provided;
+ */
+function session_data($our_user, $our_user_pass, $post_user_name, $post_password) {
+    if ($our_user === stripslashes($post_user_name) && $our_user_pass === stripslashes($post_password)) {
+        $_SESSION['user'] = $post_user_name;
+        $_SESSION['password'] = $post_password;
+        return 1;
     } else {
         return "<p style='color:red;'>Wrong pass or name</p>";
     }
 }
 
+/*
+ * check_data checking is the name and password in session equals to data in database
+ * @params $our_user        - user name from database;
+ * @params $our_user_pass   - user password from database;
+ * return number            - imitates true or false;
+ */
 function check_data($our_user, $our_user_pass) {
-    if ($_SESSION['user'] ===  $our_user && $_SESSION['password'] === $our_user_pass) {
+    $session_name = $_SESSION['user'];
+    $session_pass = $_SESSION['password'];
+    if ($session_name ===  $our_user &&  $session_pass=== $our_user_pass) {
         return 1;
     } else {
         return 0;
@@ -40,7 +65,12 @@ function check_data($our_user, $our_user_pass) {
 }
 
 if  (isset($_POST["submit"])){
-    echo submit_form($our_user, $our_user_pass, $_POST["user_name"], $_POST["password"]);
+    if (submit_form($our_user, $our_user_pass, $_POST["user_name"], $_POST["password"]) === 1) {
+        header("Location: welcome_page.php");
+    } else {
+        echo submit_form($our_user, $our_user_pass, $_POST["user_name"], $_POST["password"]);
+    }
+
 }
 if (isset($_POST["destroy"])) {
     echo "destroy";
--- tests/function.php
+++ tests/function.php
@@ -9,6 +9,47 @@ use PHPUnit\Framework\TestCase;
 
 require("../function.php");
 
-class StackTest extends TestCase {
+class StackTest extends TestCase
+{
+    const OUR_USER = "Vasile";
+    const OUR_USER_PASS = "Hello123";
+    const POST_USER_NAME = "";
+    const POST_PASSWORD = "hello";
 
+    /*
+     * testsubmit_form_success test for fail message if function success
+     */
+    public function testsubmit_form_success()
+    {
+        $result = submit_form(self::OUR_USER, self::OUR_USER_PASS, self::OUR_USER, self::POST_PASSWORD);
+        $expect = "<p style='color:red;'>Wrong pass or name</p>";
+        $this -> assertEquals($expect, $result);
+    }
+    /*
+    * testsubmit_form_fail test is the main function return request to provide name and password
+    */
+    public function testsubmit_form_fail()
+    {
+        $result = submit_form(self::OUR_USER, self::OUR_USER_PASS, self::POST_USER_NAME, self::POST_PASSWORD);
+        $expect = "<p style='color:red;'>Please provide login and pass</p>";
+        $this -> assertEquals($expect, $result);
+    }
+    /*
+     * testsession_data_success - if name and pass equals in database and provided one
+     * main function should return 1(true)
+     */
+    public function testsession_data_success() {
+        $result = session_data(self::OUR_USER, self::OUR_USER_PASS, self::OUR_USER, self::OUR_USER_PASS);
+        $expect = 1;
+        $this -> assertEquals($expect, $result);
+    }
+    /*
+     * testsession_data_success - if name and pass not equals in database and provided one
+     * main function should return 0(false)
+     */
+    public function testsession_data_fail() {
+        $result = session_data(self::OUR_USER, self::OUR_USER_PASS, self::POST_USER_NAME, self::OUR_USER_PASS);
+        $expect = "<p style='color:red;'>Wrong pass or name</p>";
+        $this -> assertEquals($expect, $result);
+    }
 }
